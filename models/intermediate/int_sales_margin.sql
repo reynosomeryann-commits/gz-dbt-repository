e15d7b2c25d9c491a247d0a673449027273c53da
Mery Ann Reynoso Almonte <reynosomeryann@gmail.com>
2026-02-11 17:16:14 +0000

    margin and purchase_cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+select  
+    sales.orders_id,
+    sales.products_id,
+    sales.quantity,
+    sales.revenue,
+    product.purchase_price,
+    (CAST(sales.quantity AS INT64) * CAST(product.purchase_price AS FLOAT64)) AS purchase_cost,
+    (CAST(sales.revenue AS FLOAT64) - (CAST(sales.quantity AS INT64) * CAST(product.purchase_price AS FLOAT64))) AS margin
+from {{ref("stg_raw__sales")}} as sales
+LEFT JOIN {{ref("stg_raw__product")}} as product
+ON sales.products_id = product.products_id
